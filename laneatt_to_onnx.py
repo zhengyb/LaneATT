accc9cfb964ea69a892ee1e8529260fc4873c188
zhengyb <zhengyb@inhand.com.cn>
2025-05-21 10:42:22 -0400

    reset invlid feature with 1d array

--- laneatt_to_onnx.py
+++ laneatt_to_onnx.py
@@ -35,19 +35,23 @@ class LaneATTONNX(torch.nn.Module):
         #[704000, 1]
         #self.indices = self.cut_xs + 20 * self.cut_ys + 12 * 20 * self.cut_zs
         #print(f"self.indices.shape: {self.indices.shape}")
-        indices = self.cut_xs + self.fmap_w * self.cut_ys + self.fmap_h * self.fmap_w * self.cut_zs
-        indices = indices.contiguous().view(-1)  # 将indices展平为一维向量
-        print(f"self.indices.shape: {indices.shape}")
-        indices = indices.contiguous()
-        self.register_buffer('indices', indices)
+        #indices = self.cut_xs + self.fmap_w * self.cut_ys + self.fmap_h * self.fmap_w * self.cut_zs
+        #indices = indices.contiguous().view(-1)  # 将indices展平为一维向量
+        #print(f"self.indices.shape: {indices.shape}")
+        #indices = indices.contiguous()
+        #self.register_buffer('indices', indices)
         # register indices as a buffer
-
+        print(f"self.invalid_mask.shape: {self.invalid_mask.shape}")
+        # reshape invalid mask to (704000, 1)
+        self.reshaped_invalid_mask = model.invalid_mask.view(-1, 1)
+        print(f"self.reshaped_invalid_mask.shape: {self.reshaped_invalid_mask.shape}")
 
     def simple_cut_features(self, batch_features):
         indices = self.cut_xs + 20 * self.cut_ys + 12 * 20 * self.cut_zs
         # 使用预定义的索引选择特定位置的特征
-        batch_anchor_features = batch_features[:, self.indices].\
-            view(-1, 1000, self.anchor_feat_channels, self.fmap_h, 1)
+        #batch_anchor_features = batch_features[:, indices].\
+        #    view(-1, 1000, self.anchor_feat_channels, self.fmap_h, 1)
+        batch_anchor_features = batch_features[:, indices]
         return batch_anchor_features
 
     def forward(self, x):
@@ -68,11 +72,17 @@ class LaneATTONNX(torch.nn.Module):
         batch_anchor_features = self.simple_cut_features(batch_anchor_features)
         #b2
         
+        # bim
         # batch_anchor_features[self.invalid_mask] = 0
         # 应用无效掩码，将无效区域的特征置为0
         #batch_anchor_features = batch_anchor_features * torch.logical_not(
         #    self.invalid_mask
         #)
+        # bim1d
+        batch_anchor_features = batch_anchor_features * torch.logical_not(
+            self.reshaped_invalid_mask
+        )
+
 
         # Join proposals from all images into a single proposals features batch
         # batchx1000x704
@@ -80,7 +90,6 @@ class LaneATTONNX(torch.nn.Module):
         batch_anchor_features = batch_anchor_features.view(
             -1, 1000, self.anchor_feat_channels * self.fmap_h
         )
-
         #b3
 
         # Add attention features
@@ -167,8 +176,8 @@ def export_onnx(onnx_file_path):
     except Exception as e:
         print(f"simplifier failure: {e}")
 
-    onnx.save(model_onnx, "LaneATT_test.sim-b5-attention.onnx")
-    print(f"simplify done. onnx model save in LaneATT_test.sim-b5-attention.onnx")
+    onnx.save(model_onnx, "LaneATT_test.sim-bim1d.onnx")
+    print(f"simplify done. onnx model save in LaneATT_test.sim-bim1d.onnx")
 
 
 if __name__ == "__main__":
